3e72fc73bb7f8ad9d2c99d4d6269d69026073c43
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-28 18:15:41 -0500

    player loaded in map

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ canvas.height = 576;
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image();
-image.src = "./assets/Pellet-Town-Zoomed.png";
+const mapImage = new Image();
+mapImage.src = "./img/Pellet-Town-Zoom-Help.png";
 
-image.onload = () => {
-  c.drawImage(image, 0, 0);
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
+mapImage.onload = () => {
+  c.drawImage(mapImage, -735, -610);
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
