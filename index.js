9dd0dc40cfa018dd27dd515dfcedc3884e37aaff
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-28 17:47:16 -0500

    player map loaded in

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const canvas = document.querySelector("canvas");
-const c = canvas.getContext('2d')
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "./assets/Pellet-Town-Zoomed.png";
+
+image.onload = () => {
+  c.drawImage(image, 0, 0);
+};
