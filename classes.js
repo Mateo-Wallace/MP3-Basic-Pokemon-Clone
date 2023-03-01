a4ebdafa0dbea6b612ac9e8e1873314481a58e16
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-01 16:10:15 -0500

    fixed monster position change on death

--- classes.js
+++ classes.js
@@ -31,7 +31,7 @@ class Sprite {
       this.width = this.image.width / this.frames.max;
       this.height = this.image.height;
     };
-    this.image.src = image.src
+    this.image.src = image.src;
     this.animate = animate;
     this.sprites = sprites;
     this.opacity = 1;
@@ -202,10 +202,15 @@ class Monster extends Sprite {
       "#dialogueBox"
     ).innerHTML = `<p>${this.name} fainted!<p> <p>Click...<p>`;
     gsap.to(this.position, {
-      y: this.position.y+20
-    })
+      y: this.position.y + 20,
+    });
     gsap.to(this, {
-      opacity: 0
-    })
+      opacity: 0,
+      onComplete: () => {
+        gsap.to(this.position, {
+          y: this.position.y - 20,
+        });
+      },
+    });
   }
 }
