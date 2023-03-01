9d63645d5a121f84fb706d7a14c37d15ab258905
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-28 23:35:47 -0500

    foreground objects rendering

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
   showBoundaries: 0,
-  
+
   images: {
-    map: './img/Pellet-Town-Zoom-Help.png',
-    playerDown: './img/playerDown.png'
-  }
+    map: "./img/Pellet-Town-Zoom-Help.png",
+    foregroundObjects: "./img/foregroundObjects.png",
+    playerDown: "./img/playerDown.png",
+  },
 };
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // gets the html for the screen
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-
 // determines size for game window
 canvas.width = 1024;
 canvas.height = 576;
@@ -11,30 +10,12 @@ const collisionsMap = [];
 for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i));
 }
-
-// defines what a boundary is
-class Boundary {
-  static width = 48;
-  static height = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48;
-    this.height = 48;
-  }
-
-  draw() {
-    c.fillStyle = `rgba(255, 0, 0, ${config.showBoundaries ? "0.3" : "0"})`;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 // array to hold every row of boundaries
 const boundaries = [];
 const offset = {
   x: -735,
   y: -650,
 };
-
 // pushes each row of boundaries into an array with defined positioning
 collisionsMap.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -56,33 +37,9 @@ mapImage.src = config.images.map;
 // loads the player model image asset
 const playerImage = new Image();
 playerImage.src = config.images.playerDown;
-
-// sets a class for images to animate
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-    };
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
+// loads foreground image assets such as buildings and trees
+const foregroundImage = new Image();
+foregroundImage.src = config.images.foregroundObjects;
 
 // defines the player image as a sprite
 const player = new Sprite({
@@ -95,7 +52,6 @@ const player = new Sprite({
     max: 4,
   },
 });
-
 // defines the background image as a sprite
 const background = new Sprite({
   position: {
@@ -103,6 +59,14 @@ const background = new Sprite({
     y: offset.y,
   },
   image: mapImage,
+});
+// defines foreground image as a sprite
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
 });
 
 // tracks whether a key has been pressed
@@ -122,7 +86,7 @@ const keys = {
 };
 
 // determines what objects move on player key press
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries, foreground];
 // determines if player and boundaries are overlapping
 const rectangularCollision = ({ rectangle1, rectangle2 }) => {
   return (
@@ -146,6 +110,7 @@ const animate = () => {
 
   // draws player on screen
   player.draw();
+  foreground.draw();
 
   let moving = true;
   // adjusts moveables position based on movement input
