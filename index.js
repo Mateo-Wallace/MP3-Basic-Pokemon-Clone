40379681284a8e528aad2fc597ef55146303e120
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-01 11:53:08 -0500

    refactored code, moved all battle code into a seperate file

--- index.js
+++ index.js
@@ -97,50 +97,6 @@ const foreground = new Sprite({
   image: foregroundImage,
 });
 
-// loads all battle images
-const battleBackgroundImage = new Image();
-battleBackgroundImage.src = config.images.battleBackground;
-const draggleImage = new Image();
-draggleImage.src = config.images.draggle;
-const embyImage = new Image();
-embyImage.src = config.images.emby;
-
-// loads all battle sprites
-const battleBackground = new Sprite({
-  position: {
-    x: 0,
-    y: 0,
-  },
-  image: battleBackgroundImage,
-});
-const draggle = new Sprite({
-  position: {
-    x: 800,
-    y: 100,
-  },
-  image: draggleImage,
-  frames: {
-    max: 4,
-    hold: 30,
-  },
-  animate: true,
-  isEnemy: true,
-  name: "Draggle",
-});
-const emby = new Sprite({
-  position: {
-    x: 280,
-    y: 325,
-  },
-  image: embyImage,
-  frames: {
-    max: 4,
-    hold: 30,
-  },
-  animate: true,
-  name: "Emby",
-});
-
 // tracks whether a key has been pressed
 const keys = {
   w: {
@@ -355,29 +311,6 @@ const animate = () => {
       });
   }
 };
-
-const renderedSprites = [draggle, emby];
-const animateBattle = () => {
-  window.requestAnimationFrame(animateBattle);
-  battleBackground.draw();
-
-  renderedSprites.forEach((sprite) => {
-    sprite.draw();
-  });
-};
-config.onlyBattle ? animateBattle() : animate();
-
-// listens for battle input
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML];
-    emby.attack({
-      attack: selectedAttack,
-      recipient: draggle,
-      renderedSprites,
-    });
-  });
-});
 
 // listens for player movement input
 let lastKey = "";
