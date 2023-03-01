2a79743f665e8c3b4c27cc72d7544301fb6744ed
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-01 00:00:36 -0500

    player turns when walking in new direction

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ const animate = () => {
   // adjusts moveables position based on movement input
   if (keys.w.pressed && lastKey === "w") {
     player.moving = true;
+    player.image = player.sprites.up;
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i];
       if (
@@ -154,6 +155,7 @@ const animate = () => {
       });
   } else if (keys.a.pressed && lastKey === "a") {
     player.moving = true;
+    player.image = player.sprites.left;
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i];
       if (
@@ -179,6 +181,7 @@ const animate = () => {
       });
   } else if (keys.s.pressed && lastKey === "s") {
     player.moving = true;
+    player.image = player.sprites.down;
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i];
       if (
@@ -204,6 +207,7 @@ const animate = () => {
       });
   } else if (keys.d.pressed && lastKey === "d") {
     player.moving = true;
+    player.image = player.sprites.right;
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i];
       if (
