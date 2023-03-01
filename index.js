fb0a71d28bedd424c93598b83976eb9de43d2eeb
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-01 00:36:10 -0500

    battle zone track when player walks on them

--- index.js
+++ index.js
@@ -146,6 +146,33 @@ const animate = () => {
   player.draw();
   foreground.draw();
 
+  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < battleZones.length; i++) {
+      const battleZone = battleZones[i];
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          battleZone.position.x + battleZone.width
+        ) -
+          Math.max(player.position.x, battleZone.position.x)) *
+        (Math.min(
+          player.position.y + player.height,
+          battleZone.position.y + battleZone.height
+        ) -
+          Math.max(player.position.y, battleZone.position.y));
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: battleZone,
+        }) &&
+        overlappingArea > (player.width * player.height) / 2
+      ) {
+        console.log("battle zone collision");
+        break;
+      }
+    }
+  }
+
   let moving = true;
   player.moving = false;
   // adjusts moveables position based on movement input
